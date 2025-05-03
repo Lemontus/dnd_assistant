ef94792962aa36254073870b42464de853570122
Lemontus <legitaspug@gmail.com>
2025-05-03 13:41:15 +0200

    Add the function to print list of servers/members

--- bot.py
+++ bot.py
@@ -5,13 +5,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
+# TypeError, found fix here https://shorturl.at/ALSsg
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    # loops through client.guilds to find the guild(server) provided in .env
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    # loops through the guild.members then prints out the members inside the guild(server)
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)

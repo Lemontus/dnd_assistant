65a46fc7674dc2d28dca54c20353080bb8d0b698
Lemontus <legitaspug@gmail.com>
2025-05-03 16:42:51 +0200

    Create create channel functionality

--- bot.py
+++ bot.py
@@ -39,6 +39,21 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='new channel'):
+    guild = ctx.guild
+    existing_channel =discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+
 bot.run(TOKEN)
 
 # TypeError, found fix here https://shorturl.at/ALSsg, better way to write it here https://www.reddit.com/r/discordbots/comments/11svqp6/bot_doesnt_recognize_or_respond_to_prefix/
